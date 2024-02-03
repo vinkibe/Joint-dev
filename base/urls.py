6985d0023b23b495990543655ca4aacc2a658e2b
vin <vin@vin.com>
2024-02-03 13:41:23 +0300

    master

---
+++ base/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerPage, name='register'),
+
+    path('', views.home, name='home'),
+    path('room/<str:id>/', views.room, name='room'),
+    path('profile/<str:id>/', views.userProfile, name='user-profile'),
+    path('join-meet/', views.joinMeeting, name='join-meet'),
+
+    path('create-room/', views.createRoom, name='create-room'),
+    path('update-room/<str:id>/', views.updateRoom, name='update-room'),
+    path('delete-room/<str:id>/', views.deleteRoom, name='delete-room'),
+    path('delete-message/<str:id>/', views.deleteMessage, name='delete-message'),
+
+
+]
